5c3db4ed99e29d9d216684afed93f4e67d8baa60
Sukairo-02 <sreka9056@gmail.com>
2024-06-11 11:43:50 +0300

    Added version with fixed issue

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,36 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
-import { cors } from 'hono/cors'
 
 const app = new Hono()
 
 app.use(
 	'*',
-	cors({
-		origin: '*',
-		allowMethods: ['GET', 'PUT', 'POST', 'OPTIONS'],
-		allowHeaders: ['*']
-	})
+	async (c, next) => {
+		c.res.headers.set('Access-Control-Allow-Headers', '*')
+		c.res.headers.set('Access-Control-Allow-Methods', 'GET,PUT,POST,OPTIONS')
+		c.res.headers.set('Access-Control-Allow-Origin', '*')
+
+		if (c.req.header('Access-Control-Request-Private-Network') === 'true') {
+			c.res.headers.set('Access-Control-Allow-Private-Network', 'true')
+		}
+
+		return await next()
+	}
 )
+
+app.options('*', (c) => {
+    if(c.req.url.split('/').pop() === '') {
+        c.res.headers.delete('Access-Control-Allow-Private-Network')
+    }
+
+	return c.text('OK')
+})
 
 app.get('/', (c) => {
 	return c.text('Connected')
 })
 
 app.get('/headers', (c) => {
-    c.res.headers.set('Access-Control-Allow-Private-Network', 'true')
-	c.res.headers.set('Content-Security-Policy', 'treat-as-public-address')
-
 	return c.text('Connected')
 })
 
