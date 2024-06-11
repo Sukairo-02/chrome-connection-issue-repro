325d4da871cf40074dcec4c038fbe9c9bdb5c101
Sukairo-02 <sreka9056@gmail.com>
2024-06-11 05:01:46 +0300

    Fixed scripts, cors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 
 const app = new Hono()
+
+app.use('*', cors({
+ origin: '*'
+}))
 
 app.get('/', (c) => {
 
