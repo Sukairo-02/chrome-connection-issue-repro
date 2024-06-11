ab1f80d794ca343e99a1f6a000175841b8350f8c
Sukairo-02 <sreka9056@gmail.com>
2024-06-11 11:22:43 +0300

    Attempt at fixing

--- src/index.ts
+++ src/index.ts
@@ -4,19 +4,31 @@ import { cors } from 'hono/cors'
 
 const app = new Hono()
 
-app.use('*', cors({
- origin: '*'
-}))
+app.use(
+	'*',
+	cors({
+		origin: '*',
+		allowMethods: ['GET', 'PUT', 'POST', 'OPTIONS'],
+		allowHeaders: ['*']
+	})
+)
+
+app.use('/headers', (c, next) => {
+	c.header('Access-Control-Allow-Private-Network', 'true')
+	c.header('Content-Security-Policy', 'treat-as-public-address')
+
+	return next()
+})
 
 app.get('/', (c) => {
-    return c.text('Connected')
+	return c.text('Connected')
 })
 
 app.get('/headers', (c) => {
-    return c.text('Connected')
+	return c.text('Connected')
 })
 
 serve({
-    port: Number(process.argv[2]),
-    fetch: app.fetch
+	port: Number(process.argv[2]),
+	fetch: app.fetch
 })
